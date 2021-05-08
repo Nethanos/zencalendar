53ac4caed3bb152f513244dfc1b994d1dabf24ca
nethanos <n.santos1897@gmail.com>
2021-05-08 17:01:45 -0300

    [wip][feat]: finishing initial part of professional summary

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'zen-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
 
   constructor() { }
+
+  reviewQuantity = 20;
+
+  starQuantity(quantity: number): Array<number> {
+    return new Array(quantity);
+  }
 
   ngOnInit(): void {
   }
